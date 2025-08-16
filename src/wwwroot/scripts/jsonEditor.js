7f8ae1ae89c7607bcc216ca731d14120781f33fd
ziagham <amin.ziagham@outlook.com>
2025-08-16 23:28:08 +0200

    Add jsoneditor to the WorkFlowJsonEditor compoennt #34

---
+++ src/wwwroot/scripts/jsonEditor.js
@@ -0,0 +1,29 @@
+﻿window.initializeJsonEditor = function (element, initialJson, dotNetRef) {
+    const options = {
+        mode: 'code',
+        modes: ['code', 'view'],
+        enableSort: false,
+        enableTransform: false,
+        enableFilter: false,
+        onChange: function () {
+            try {
+                const json = editor.get();
+                dotNetRef.invokeMethodAsync('OnJsonChange', JSON.stringify(json));
+            } catch (err) {
+                console.error('Error parsing JSON', err);
+            }
+        }
+    };
+
+    const editor = new JSONEditor(element, options);
+    if (initialJson) {
+        try {
+            editor.set(JSON.parse(initialJson));
+        } catch (err) {
+            console.error('Error setting initial JSON', err);
+            editor.setText(initialJson);
+        }
+    }
+
+    return editor;
+};
