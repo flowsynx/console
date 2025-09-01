e29bfed4a1f28109efbf1298a49d8a8534ebca61
ziagham <amin.ziagham@outlook.com>
2025-09-01 22:18:39 +0200

    Add Monitoring Dialog as well as Monitoring component #60

---
+++ src/Models/WorkflowExecutionUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Console.Models;
+
+public class WorkflowExecutionUpdateDto
+{
+    public Guid WorkflowId { get; set; }
+    public Guid ExecutionId { get; set; }
+    public DateTime ExecutionStart { get; set; }
+    public DateTime? ExecutionEnd { get; set; }
+    public string Status { get; set; } = "";
+}
